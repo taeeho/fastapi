0d398971a2ac9a3e6ff89952059b79bb6a3ec933
taeho <htaeho925@gmail.com>
2025-09-08 16:21:58 +0900

    frontend front

---
+++ frontend/front/src/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const App = () => {
+  return (
+    <div className='flex items-center justify-center min-h-screen bg-white text-black'>
+      <h1 className='text-2xl font-bold'>Hello, React</h1>
+      
+    </div>
+  );
+};
+
+export default App;
