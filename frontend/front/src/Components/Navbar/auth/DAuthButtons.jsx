49d53f231d5d45310500a52b782a2333f2ae842c
taeho <htaeho925@gmail.com>
2025-09-09 10:05:38 +0900

    edit index.jsx, auth

--- frontend/front/src/Components/Navbar/auth/DAuthButtons.jsx
+++ frontend/front/src/Components/Navbar/auth/DAuthButtons.jsx
@@ -1,7 +1,11 @@
+import { Link } from "react-router-dom";
+
+
 const DAuthButtons = ({
   isAuthenticated,
   isOpen,
   setIsOpen,
+  onLogoutClick,
   onLoginClick,
   onSignupClick,
 }) => {
@@ -13,43 +17,42 @@ const DAuthButtons = ({
             onClick={() => setIsOpen(!isOpen)}
             className="flex items-center focus:outline-none"
           >
-           
           </button>
 
-          {/* {isOpen && (
-            <div className="absolute right-0 mt-2 w-48 bg-white rounded-md shadow-lg py-1 z-10">
-              <Link
-                to="/profile"
-                className="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100"
-              >
-                프로필
-              </Link>
-              <Link
-                to="/contact"
-                className="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100"
-              >
-                문의하기
-              </Link>
+          {isOpen && (
+            // <div className="absolute right-0 mt-2 w-48 bg-white rounded-md shadow-lg py-1 z-10">
+            //   <Link
+            //     to="/profile"
+            //     className="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100"
+            //   >
+            //     프로필
+            //   </Link>
+            //   <Link
+            //     to="/contact"
+            //     className="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100"
+            //   >
+            //     문의하기
+            //   </Link>
               <button
                 onClick={onLogoutClick}
                 className="block w-full text-left px-4 py-2 text-sm text-gray-700 hover:bg-gray-100"
               >
                 로그아웃
               </button>
-            </div>
-          )} */}
+            // </div>
+          )}
         </div>
       ) : (
         <div className="flex space-x-4">
           <button
             onClick={onLoginClick}
-            className="text-white hover:bg-purple-300 px-3 py-2 rounded-md text-sm font-medium transition-all duration-500"
+            className="text-white hover:bg-purple-600 px-3 py-2 rounded-md text-sm font-medium transition-all duration-500"
           >
             로그인
           </button>
           <button
             onClick={onSignupClick}
-            className="text-purple-200 bg-white hover:bg-gray-100 px-3 py-2 rounded-md text-sm font-medium transition-all duration-500"
+            className="text-purple-500 bg-white hover:bg-gray-100 px-3 py-2 rounded-md text-sm font-medium transition-all duration-500"
           >
             회원가입
           </button>
--- frontend/front/src/Components/Navbar/index.jsx
+++ frontend/front/src/Components/Navbar/index.jsx
@@ -1,11 +1,11 @@
 import { useState } from "react";
 import Logo from "./layout/Logo";
 import AuthButtons from "./auth/DAuthButtons";
+import MobileMenu from "./layout/MobileMenu";
 
 function Navbar() {
-	const isAuthenticated = false;
   const [isOpen, setIsOpen] = useState(false);
-  
+  const isAuthenticated = false;
 
   const onLoginClick = () => {
     alert("로그인");
@@ -23,7 +23,17 @@ function Navbar() {
     <nav className="bg-purple-500">
       <div className="max-w-full mx-auto px-4 sm:px-6 lg:px-8">
         <div className="flex items-center justify-between h-14 md:h-20">
-          <div className="flex items-center flex-1"></div>
+          <div className="flex items-center flex-1">
+            <Logo />
+          </div>
+          <AuthButtons
+            isAuthenticated={isAuthenticated}
+            isOpen={isOpen}
+            setIsOpen={setIsOpen}
+            onLogoutClick={onLogoutClick}
+            onLoginClick={onLoginClick}
+            onSignupClick={onSignupClick}
+          />
         </div>
       </div>
     </nav>
