ddcc23a87bb744c2d33a6ca80b104d6cd068d0d2
taeho <htaeho925@gmail.com>
2025-09-09 10:07:47 +0900

    mobileMenu

--- frontend/front/src/Components/Navbar/layout/MobileMenu.jsx
+++ frontend/front/src/Components/Navbar/layout/MobileMenu.jsx
@@ -2,7 +2,13 @@ import React from "react";
 import { BiSearch } from "react-icons/bi";
 
 const MobileMenu = ({
+  user,
   isOpen,
+  setIsOpen,
+  isAuthenticated,
+  onLoginClick,
+  onLogoutClick,
+  onSigupClick,
 }) => {
   return (
     <div
